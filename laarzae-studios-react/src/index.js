d37656e660b2595d47bf11e74c2ff40cc1c45fb4
ngwatso <ngwatson79@yahoo.com>
2021-03-15 18:56:16 -0400

    created components for main content

--- laarzae-studios-react/src/index.js
+++ laarzae-studios-react/src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+// !! Placeholder reducer
+const dummyReducer = () => {};
+
+// !! App-level state
+const store = createStore(dummyReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
