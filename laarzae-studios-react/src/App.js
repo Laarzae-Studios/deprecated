04f1788912594965ccaa1fa9b97499ee325ec66e
ngwatso <ngwatson79@yahoo.com>
2021-03-15 20:43:29 -0400

    added background color, font color, fonts

--- laarzae-studios-react/src/App.js
+++ laarzae-studios-react/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch, Link, Redirect } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-        <h1> Hello, world! </h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* // TODO Login, Register, Logout links to be used later */}
+			{/* <Link to="/login">Log In</Link>
+      <Link to="/register">Sign Up</Link> 
+      <Link to="/login" onClick={() => {localStorage.clear()}>Log Out</Link>*/}
+			<Header />
+			<h1> Hello, world! </h1>
+		</div>
+	);
 }
 
 export default App;
---
+++ laarzae-studios-react/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div className="header">
+			<img
+				src="/logo.png"
+				alt="Laarzae Studios"
+				width="15%"
+				height="30%"
+			/>
+		</div>
+	);
+};
+
+export default Header;
