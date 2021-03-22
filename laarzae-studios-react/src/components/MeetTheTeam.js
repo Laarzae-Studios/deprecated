a13794de83e4068eca664b54b663711c20110a6c
ngwatso <ngwatson79@yahoo.com>
2021-03-22 14:48:56 -0400

    render issue resolved

--- laarzae-studios-react/src/components/MeetTheTeam.js
+++ laarzae-studios-react/src/components/MeetTheTeam.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const MeetTheTeam = () => {
-	const ourTeam = [];
 	const [state, setState] = useState([]);
 
 	// ?? Team array
@@ -17,45 +16,52 @@ const MeetTheTeam = () => {
 		'kdneldor',
 	];
 
-	// ?? Do axios get, utilizing teamArr.forEach, to pull users
+	// ?? Do axios get, utilizing teamArr.map, to pull users
 	useEffect(() => {
-		teamArr.forEach((userName) => {
-			axios.get(`https://api.github.com/users/${userName}`)
+		teamArr.map((userName) => {
+			return axios
+				.get(`https://api.github.com/users/${userName}`)
 				.then((res) => {
-					// ?? Push results to ourTeam array
-					ourTeam.push(res.data);
+					// ?? Set state
+					setState((state) => [...state, res.data]);
 				})
-				.catch((err) =>
-					console.error('users not found', err.message)
-				);
-			setState(ourTeam);
+				.catch((err) => {
+					console.error('users not found', err.message);
+				});
 		});
-		// ?? Set ourTeam array to state
-		console.log('ourTeam array =====> ', ourTeam);
-		console.log('state - inside useEffect =====> ', state);
 	}, []);
 
 	console.log('state - outside useEffect =====> ', state);
 
 	return (
-		<div className="meet-the-team">
+		<div className="meet-container">
 			<h2>Meet The Team!</h2>
-			{state.map((user) => {
-				return (
-					<div key={user.id} className="user-img-info">
-						<div className="user-img-container">
-							<img
-								className="user-img"
-								src={user.avatar_url}
-								alt={user.login}
-							/>
+
+			<div className="meet-the-team">
+				{state.map((user) => {
+					return (
+						<div key={user.id} className="user-img-info">
+							<div className="img-frame">
+								<div className="user-img-container">
+									<img
+										className="user-img"
+										src={user.avatar_url}
+										alt={user.login}
+									/>
+								</div>
+							</div>
+							<div className="user-info">
+								<div className="user-name">
+									{user.name}
+								</div>
+								<div className="user-quote">
+									Placeholder Text
+								</div>
+							</div>
 						</div>
-						<div className="user-info">
-							<div className="user-name">{user.name}</div>
-						</div>
-					</div>
-				);
-			})}
+					);
+				})}
+			</div>
 		</div>
 	);
 };
