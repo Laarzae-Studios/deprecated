d3d4552c4872e14b1f6772eeddd54a68e58e29e1
ClydeFrog04 <egan.randal@gmail.com>
2021-04-03 12:54:25 -0600

    feat: added cleaned up home page component

--- laarzae-studios-react/src/App.js
+++ laarzae-studios-react/src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import {Route, Switch} from "react-router-dom";
 import Header from "./components/Header";
 import MeetTheTeam from "./components/MeetTheTeam";
-import Homepage from "./components/Homepage";
+import Homepage from "./components/HomePageClean";
+import HomePageClean from "./components/HomePageClean.jsx";
 
 function App() {
     return (
---
+++ laarzae-studios-react/src/components/HomePageClean.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const HomePageClean = () =>{
+
+
+
+    return (
+        <div className="home-container">
+        </div>
+    );
+}
+
+export default HomePageClean;
