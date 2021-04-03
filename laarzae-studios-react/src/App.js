7a695f029aed09882c13c8844136aaf544bbb6b1
ClydeFrog04 <egan.randal@gmail.com>
2021-04-03 12:43:56 -0600

    refactor(j): removed unused imports

--- laarzae-studios-react/src/App.js
+++ laarzae-studios-react/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import {Route, Switch, Link, Redirect} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import Header from "./components/Header";
 import MeetTheTeam from "./components/MeetTheTeam";
 import Homepage from "./components/Homepage";
